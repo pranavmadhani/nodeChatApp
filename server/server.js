8603dacfa35871bf1367b56c50c672cbb8da38b1
pranav madhani <you@example.com>
2019-08-18 10:45:32 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const publicPath = path.join(__dirname,'../public')
+const PORT = process.env.PORT||3000;
+console.log(publicPath)
+
+  app.use(express.static(publicPath))
+
+
+
+
+app.listen(PORT,()=>{
+
+    console.log('server started at',PORT)
+})
+
+
