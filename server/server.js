860d2bc5103e97f15b3cd3da6289831c51b426b2
pranav madhani <you@example.com>
2019-09-01 11:26:39 +0530

    user list added

--- server/server.js
+++ server/server.js
@@ -1,79 +1,59 @@
-const path = require('path')
-const express = require('express')
-const http = require('http')
-const app = express()
-const {
-    generateMessage,
-    generateLocationMessage
-} = require('./utils/mesage')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '../public')
-console.log(publicPath)
-const PORT = process.env.PORT || 3000;
-const socketIO = require('socket.io')
-var server = http.createServer(app)
-var io = socketIO(server)
-app.use(express.static(publicPath))
-const {isRealString} = require('./utils/validator')
-const {Users} = require('./utils/users')
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 var users = new Users();
 
+app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required.');
+    }
 
-io.on('connection', (socket) => { //reserved keyword
-    console.log('new user connected')
-   
-   
-   
-    socket.on('disconnect', () => { //reserved keyword
-        console.log('dis-connected')
-        var user = users.removeUser(socket.id);
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
 
-        if(user)
-        {
-            io.to(user.room).emit('updateUserList',users.getUserList(user.room));
-            io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left`));
-        }
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
 
-    })
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
 
-    
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 
-    socket.on('join',(params,callback)=>{
+  socket.on('disconnect', () => {
+    var user = users.removeUser(socket.id);
 
-        if(!isRealString(params.name) || !isRealString(params.room)){
-            callback('error! name and room name are required.')
-        }
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
+  });
+});
 
-        socket.join(params.room)
-        users.removeUser(socket.id);
-        users.addUser(socket.id,params.name,params.room);
-        io.to(params.room).emit('updateUserList',users.getUserList(params.room))
-
-        socket.emit('newMessage', generateMessage('Admin', 'welcome to the chat app')
-    )
-    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name}  has joinned the room...`))
-        callback(); 
-
-    })
-
-
-    socket.on('createMessage', function (newMsg,callback) {
-        console.log('create Message', newMsg);
-        io.emit('newMessage', generateMessage(newMsg.from, newMsg.text))
-        callback('this is from server');
-            
-    })
-
-    socket.on('createLocationMessage',function(coords){
-        io.emit('newLocationMessage',generateLocationMessage(`Admin`,coords.latitude,coords.longitude))
-        
-    })
-   
-})
-
-
-server.listen(PORT, () => {
-    console.log('server started at', PORT)
-})
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
--- server/utils/users.js
+++ server/utils/users.js
@@ -1,33 +1,47 @@
+
+
+
 class Users {
-    constructor() {
-        this.users = [];
+  constructor () {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
     }
-    addUser(id, name, room) {
-        var user = {
-            id,
-            name,
-            room
-        }
-        this.users.push(user);
-        return user;
-    }
-    removeUser(id) {
-        var user = this.getUser(id);
-       
-        if (user) {
-            this.users = this.users.filter((user) => user.id != id);
-        }
-        return user;
-    }
-    getUser(id) {
-        return this.users.filter((user) => user.id === id)[0]
-    }
-    getUserList(room) {
-        var users = this.users.filter ((user) => user.room === room);
-        var nameArray = users.map( (user) => user.name)
-        return nameArray;
-    }
+
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  }
 }
-module.exports = {
-    Users
-}
+
+module.exports = {Users};
+
+ // class Person {
+ //   constructor (name, age) {
+ //     this.name = name;
+ //     this.age = age;
+ //   }
+ //   getUserDescription () {
+ //     return `${this.name} is ${this.age} year(s) old.`;
+ //   }
+ // }
+ //
+ // var me = new Person('Andrew', 25);
+ // var description = me.getUserDescription();
+ // console.log(description);
